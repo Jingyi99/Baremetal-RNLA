f33355b527a8db7a9604f1383fdf5707f30eb18e
Jingyi Xu <jingyixu@berkeley.edu>
2024-10-29 17:01:41 -0700

    Add dense sparse gemm rvv

--- src/cpu/gemm.c
+++ src/cpu/gemm.c
@@ -1,4 +1,3 @@
-// #include <stdio.h>
 #include <stdlib.h>
 
 void gemm(float *c_matrix, const float *a_matrix, const float *b_matrix,
@@ -8,6 +7,25 @@ void gemm(float *c_matrix, const float *a_matrix, const float *b_matrix,
         for (int j = 0; j < n_dim; j++) {
             for (int k = 0; k < k_dim; k++) {
                 c_matrix[i*n_dim + j] += a_matrix[i*k_dim+k] * b_matrix[k*n_dim+j];
+            }
+        }
+    }
+}
+
+void blocked_gemm(float *c_matrix, const float *a_matrix, const float *b_matrix,
+            const unsigned int m_dim, const unsigned int n_dim,
+            const unsigned int k_dim, const unsigned int bm_dim,
+            const unsigned int bk_dim, const unsigned int bn_dim) {
+    for (int i = 0; i < m_dim; i += bm_dim) {
+        for (int j = 0; j < n_dim; j += bn_dim) {
+            for (int k = 0; k < k_dim; k += bk_dim) {
+                for (int ii = i; ii < i + bm_dim && ii < m_dim; ii++) {
+                    for (int jj = j; jj < j + bn_dim && jj < n_dim; jj++) {
+                        for (int kk = k; kk < k + bk_dim && kk < k_dim; kk++) {
+                            c_matrix[ii*n_dim + jj] += a_matrix[ii*k_dim+kk] * b_matrix[kk*n_dim+jj];
+                        }
+                    }
+                }
             }
         }
     }
