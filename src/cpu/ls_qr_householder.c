249e0a8bd11e9b4bd7a44a7a56da186fa413085b
Jingyi Xu <jingyixu@berkeley.edu>
2024-12-09 21:29:10 -0800

    Clean-up:decouple LSQR test code

--- src/cpu/ls_qr_householder.c
+++ src/cpu/ls_qr_householder.c
@@ -7,39 +7,58 @@ double house(int m, double* x, double* v);
 double* houseHolderHelper(double beta, double* v, int m);
 void test_backwarderror(double* A, double* Q, double* R, int m, int n);
 
-// double house(int m, double* x, double* v) {
-//     double sigma = 0.0;
-//     double beta = 0.0;
-//     for (int i = 1; i < m; i++) {
-//         sigma += x[i] * x[i];
+double house(int m, double* x, double* v) {
+    double sigma = 0.0;
+    double beta = 0.0;
+    for (int i = 1; i < m; i++) {
+        sigma += x[i] * x[i];
+    }
+    // memcpy(v, x, m * sizeof(double));
+    v[0] = 1; 
+    if (sigma == 0 && x[0] >= 0) {
+        beta = 0.0;
+    } else if (sigma == 0 && x[0] < 0) {
+        beta = -2.0;
+    } else {
+        double mu = sqrt(x[0]*x[0] + sigma);
+        if (x[0] <= 0) {
+            v[0] = x[0] - mu;
+        } else {
+            v[0] = -sigma / (x[0] + mu);
+            // v[0] = x[0] + mu;
+        }
+        beta = 2 * v[0] * v[0] / (sigma + v[0]*v[0]);
+        double v0 = v[0];
+        if (v0 != 0) {
+            for (int i = 0; i < m; i++) {
+                v[i] = v[i]/v0;
+            }
+        }
+        // else {
+        //     for (int i = 0; i < m; i++) {
+        //         v[i] = 0;
+        //     }
+        // }
+    }
+    return beta;
+}
+
+// double house(int m, double* x, double* v){
+//     double xNorm = 0;
+//     for (int i = 0; i < m; i++){
+//         xNorm += x[i] * x[i];
 //     }
+//     xNorm = sqrt(xNorm);
+//     double sign = (x[0] >= 0) ? 1 : -1;
 //     memcpy(v, x, m * sizeof(double));
-//     v[0] = 1; 
-//     if (sigma == 0 && x[0] >= 0) {
-//         beta = 0.0;
-//     } else if (sigma == 0 && x[0] < 0) {
-//         beta = -2.0;
-//     } else {
-//         double mu = sqrt(x[0]*x[0] + sigma);
-//         if (x[0] <= 0) {
-//             v[0] = x[0] - mu;
-//         } else {
-//             v[0] = -sigma / (x[0] + mu);
-//             // v[0] = x[0] + mu;
-//         }
-//         beta = 2 * v[0] * v[0] / (sigma + v[0]*v[0]);
-//         double v0 = v[0];
-//         if (v0 != 0) {
-//             for (int i = 0; i < m; i++) {
-//                 v[i] = v[i]/v0;
-//             }
-//         }
-//         else {
-//             for (int i = 0; i < m; i++) {
-//                 v[i] = 0;
-//             }
-//         }
+//     v[0] = x[0] + sign * xNorm;
+//     double vtv = 0;
+//     double v0 = v[0];
+//     for (int i = 0; i < m; i++){
+//         v[i] = v[i]/v0;
+//         vtv += v[i] * v[i];
 //     }
+//     double beta = 2 / vtv;
 //     return beta;
 // }
 
@@ -69,29 +88,17 @@ void houseHolderQR(double* A, int m, int n){
                 A[i*n+k] = A_sub_updated[(i-j)*(n-j) + k-j];
             }
         }
+        if (j < m) {
+            for (int k = j+1; k < m; k++) {
+                A[k*n+j] = v[k-j];
+            }
+        }
         free(A_sub);
         free(A_sub_updated);
         free(H);
         free(v);
         free(x);
     }
-}
-
-double house(int m, double* x, double* v){
-    double xNorm = 0;
-    for (int i = 0; i < m; i++){
-        xNorm += x[i] * x[i];
-    }
-    xNorm = sqrt(xNorm);
-    double sign = (x[0] >= 0) ? 1 : -1;
-    memcpy(v, x, m * sizeof(double));
-    v[0] = x[0] + sign * xNorm;
-    double vtv = 0;
-    for (int i = 0; i < m; i++){
-        vtv += v[i] * v[i];
-    }
-    double beta = 2 / vtv;
-    return beta;
 }
 
 // generate I - beta * v * vT
@@ -137,11 +144,7 @@ double* backSubstitution(double* R, double* y, int m, int n){
     // }
     double* x = (double*)malloc(n*sizeof(double));
     for (int i = n-1; i >= 0; i--){
-        if (R[i*n+i] == 0) {
-            printf("BACKSUB: Singular matrix");
-            return x;
-        }
-        float sum = 0;
+        double sum = 0;
         for (int j = i+1; j < n; j++){
             sum += R[i*n+j] * x[j];
         }
@@ -159,26 +162,18 @@ double* backSubstitution(double* R, double* y, int m, int n){
 void houseHolderQRb(double* A, double* b, int m, int n) {
     double* Acopy = (double*)malloc(m*n*sizeof(double));
     memcpy(Acopy, A, m*n*sizeof(double));
-    double* Harray[n];
-    double* Q = (double*)malloc(m*m*sizeof(double));
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < m; j++){
-            if (i == j){
-                Q[i*m+j] = 1;
-            } else {
-                Q[i*m+j] = 0;
-            }
-        }
-    }
-    FILE *f = fopen("v.txt", "w");
     for (int j = 0; j < n; j ++){
         double *v = (double*)malloc((m-j)*sizeof(double));
         double *x = (double*)malloc((m-j)*sizeof(double));
         for (int i = j; i < m; i++){
+            v[i-j] = A[i*n+j];
+        }
+        for (int i = j; i < m; i++){
             x[i-j] = A[i*n+j];
         }
+        printf("start of x: %d \n", j*n+j);
         // write v to file 
-     
+        FILE *f = fopen("v.txt", "w");
         if (f == NULL)
         {
             printf("Error opening file!\n");
@@ -187,12 +182,18 @@ void houseHolderQRb(double* A, double* b, int m, int n) {
         for (int i = j; i < m; i++){
             fprintf(f, "%f\n", A[i*n+j]);
         }
-        fprintf(f, "\n");
+        fclose(f);
         double beta = house(m-j, x, v);
+        printf("v: \n");
+        for (int i = 0; i < m; i++) {
+            printf("%lf ", v[i]);
+        }
+        printf("\n");
         double* H = houseHolderHelper(beta, v, m-j);
         double* b_sub = (double*) malloc(( m-j) * sizeof(double));
         double* b_sub_updated = (double*) malloc((m-j)*sizeof(double));
-         for (int i = 0; i < m-j; i++){
+        // memcpy(b_sub, &b[j], sizeof(double)*(m-j));
+        for (int i = 0; i < m-j; i++){
             b_sub[i] = b[i+j];
         }
         // vt*b
@@ -207,21 +208,11 @@ void houseHolderQRb(double* A, double* b, int m, int n) {
         // for (int i = j; i < m; i++){
         //     b[i] = b_sub_updated[i-j];
         // } 
-        printf("b sub:\n");
-        for (int i = 0; i < m-j; i++) {
-            printf("%lf ", b_sub[i]);
-        }
-        printf("\n");
-        printf("b sub updated:\n");
-        for (int i = 0; i < m-j; i++) {
-            printf("%lf ", b_sub_updated[i]);
-        }
-        printf("\n");
-        // memcpy(b_sub, &b[j], sizeof(double)*(m-j));
-        // gemm(b_sub_updated, H, b_sub, m-j, 1, m-j);
-        // for (int i = j; i < m; i++){
-        //     b[i] = b_sub_updated[i-j];
-        // } 
+        // printf("b sub:\n");
+        // for (int i = 0; i < m-j; i++) {
+        //     printf("%lf ", b_sub[i]);
+        // }
+        // printf("\n");
         // memcpy(&b[j], b_sub, sizeof(double)*(m-j));
         // multiply by A submatrix which is m-j * n-j
         double *A_sub = (double*)malloc((m-j)*(n-j)*sizeof(double));
@@ -237,28 +228,63 @@ void houseHolderQRb(double* A, double* b, int m, int n) {
             A[i*n+k] = A_sub_updated[(i-j)*(n-j) + k-j];
             }
         }
-        Harray[j] = H;
+        printf("A one col should be zeroed out:\n");
+        for (int ii = 0; ii < m; ii++) {
+            for (int jj = 0; jj < n; jj++) {
+                printf("%lf ", A[ii*n+jj]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+
+        
         free(A_sub);
         free(A_sub_updated);
+        free(H);
         free(v);
         free(x);
         free(b_sub);
         free(b_sub_updated);
         }
-        fclose(f);
-        double *Qresult = (double*)malloc(m*m*sizeof(double));
-        for (int i = n - 1; i >= 0; i--){
-            double* H = Harray[i];
-            gemm(Qresult, Q, H, m, m, m);
-            free(Harray[i]);
-            memcpy(Q, Qresult, m*m*sizeof(double));
-        }
-        free(Acopy);
-        free(Q);
-        free(Qresult);
-        // compute Q
-        test_backwarderror(Acopy, Q, A, m, n);
     }
+
+    // double* householderQRLS(double* A, double* b, int m, int n){
+    //     houseHolderQR(A, m, n);
+    //     printf("R:\n");
+    //     for (int i = 0; i < m; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             printf("%lf ", A[i*n+j]);
+    //         }
+    //         printf("\n");
+    //     }
+    //     for (int j = 0; j < n; j++) {
+    //         double *v = malloc(sizeof(double) * (m-j));
+    //         v[0] = 1.0;
+    //         for (int k = j+1; k < m; k++) {
+    //             v[k-j] = A[k*n+j];
+    //         }
+    //         double vtv = 0.0;
+    //         for (int i = 0; i < m-j; i++) {
+    //             vtv += v[i] * v[i];
+    //         }
+    //         double beta = 2.0/vtv;
+    //         double vtb = 0.0;
+    //         for (int i = j; i < m; i++) {
+    //             vtb += v[i-j] * b[i];
+    //         }
+    //         for (int i = j; i < m; i++) {
+    //             b[i] = b[i] - beta*vtb*v[i-j];
+    //         }
+    //     }
+    //     printf("\n");
+    //     printf("updated b:\n");
+    //     for (int i = 0; i < m; i++) {
+    //         printf("%lf ", b[i]);
+    //     }
+    //     printf("\n");
+    //     return backSubstitution(A, b, m, n);
+    // }
+
 
     double* householderQRLS(double* A, double* b, int m, int n){
         houseHolderQRb(A, b, m, n);
@@ -278,119 +304,6 @@ void houseHolderQRb(double* A, double* b, int m, int n) {
         }
         printf("\n");
         return backSubstitution(A, b, m, n);
-    }
-
-    // TODO: Move tests to another file
-    void test_house() {
-        // set test cases here
-        int m = 3;
-        double x[] = {2, 2, 1};
-        double v[] = {0, 0, 0};
-        // 
-        double beta = house(m, x, v);
-        for (int i = 0; i < m; i++) {
-        printf("x: %f, v: %f\n", x[i], v[i]);
-        }
-        printf("beta: %f\n", beta);
-    }
-
-    void test_houseHolderHelper() {
-        // set test case here
-        int m = 3;
-        double x[] = {2, 2, 1};
-        // double v[] = {5, 2, 1};
-        // double beta = 2.0 / 30.0;
-        double v[] = {1, -2, -1};
-        double beta = 1.0 / 3.0;
-        // 
-        double* result = houseHolderHelper(beta, v, m);
-
-        printf("H:\n");
-        for (int i = 0; i < m; i++){
-        for (int j = 0; j < m; j++){
-            printf("%f ", result[i*m+j]);
-        }
-        printf("\n");
-        }
-        double *Hx = malloc(sizeof(double) * m);
-        gemm(Hx, result, x, m, 1, m);
-        printf("Hx: \n");
-        for (int i = 0; i < m; i++){
-        printf("%f ", Hx[i]);
-        }
-        printf("\n");
-        free(Hx);
-    }
-
-    void test_houseHolderQR() {
-        // set test case here
-        // int m = 4;
-        // int n = 3;
-        // double A[] = {1.0, -1.0, 4.0, 1.0, 4.0, -2.0, 1.0, 4.0, 2.0, 1.0, -1.0, 0.0};
-        // int m = 3;
-        // int n = 3;
-        // double A[] = {2.0, -2.0, 18.0, 2.0, 1.0, 0.0, 1.0, 2.0, 0.0};
-        int m = 4;
-        int n = 3;
-        double A[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 10.0, 10.0, 11.0, 13.0};
-        printf("A:\n");
-        for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++){
-            printf("%f ", A[i*n+j]);
-        }
-        printf("\n");
-        }
-        houseHolderQR(A, m, n);
-        printf("R:\n");
-        for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++){
-            printf("%f ", A[i*n+j]);
-        }
-        printf("\n");
-        }
-    }
-
-    void test_backSubstitution() {
-        int m = 4;
-        int n = 3;
-        double A[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 10.0, 10.0, 11.0, 13.0};
-        double b[] = {14.0, 32.0, 50.0, 68.0};
-        double R[] = {12.884099 , 14.591630 , 16.299161 , 0.0, 1.041315 , 2.082630 , 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-        double y[] = {90.964842, 8.330522 , 0.000000 , 0.000000 };
-        householderQRLS(A, b, m, n);
-        printf("A back: \n");
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                printf("%f ", A[i*n+j]);
-            }
-            printf("\n");
-        }
-        printf("b back: \n");
-        for (int i = 0; i < m; i++){
-            printf("%f ", b[i]);
-        }
-        double *x = backSubstitution(A, b, m, n);
-        printf("x: \n");
-        for (int i = 0; i < n; i++) {
-        printf("%f ", x[i]);
-        }
-        printf("\n");
-    }
-
-    void test_houseHolderQRLS() {
-        int m = 4;
-        int n = 3;
-        double A[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 10.0, 10.0, 11.0, 13.0};
-        double b[] = {14.0, 32.0, 50.0, 68.0};
-
-        double *x = (double*) malloc(sizeof(double) * n);
-        memset(x, 0, sizeof(double) * n);
-        x = householderQRLS(A, b, m, n);
-        printf("x: \n");
-        for (int i = 0; i < n; i++) {
-        printf("%lf ", x[i]);
-        }
-        printf("\n");
     }
 
     void test_backwarderror(double* A, double* Q, double* R, int m, int n){
@@ -443,11 +356,3 @@ void houseHolderQRb(double* A, double* b, int m, int n) {
         error = residual / normA;
         printf("backward error: %f\n", error);
     }
-
-    // int main() {
-    //     // test_house();
-    //     // test_houseHolderHelper();
-    //     // test_houseHolderQR();
-    //     // test_backSubstitution();
-    //     test_houseHolderQRLS();
-    // }
