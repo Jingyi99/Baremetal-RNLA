67b48a3a7a1211f7124aee7c4679c0669fad0235
Jingyi Xu <jingyixu@berkeley.edu>
2024-12-11 19:23:15 -0800

    Full pregenerated pipeline

--- src/cpu/sketch_n_solve.c
+++ src/cpu/sketch_n_solve.c
@@ -1,21 +1,27 @@
-#include "ls_qr_householder.c"
-#include "../../dataset/sns/custom1.h"
-#include "dsgemm.c"
+// #include <riscv.h>
 
-// (float *c_matrix, const float *a_matrix,
-//             const int *b_matrix_indptr,
-//             const int *b_matrix_indices, const float *b_matrix_data,
-//             const unsigned int m_dim, const unsigned int n_dim,
-//             const unsigned int k_dim)
+#include "ls_qr_householder.c"
+#include "../../dataset/sns/sk.h"
+#include "dsgemm.c"
+#include "../../test/lib.h"
+#include "../../test/riscv.h"
+
 int main() {
+    printf("PREGENERATED SKETCH & SOLVE\n");
     float* x_ls;
     float* sketched_a_matrix = (float*)calloc(D_DIM*N_DIM, sizeof(float));
     float* sketched_b_vec = (float*)calloc(D_DIM, sizeof(float));
+    size_t sketching_start = read_cycles();
     dsgemm_csc(sketched_a_matrix, sketching_matrix, a_matrix_indptr,a_matrix_indices, a_matrix_data, D_DIM, N_DIM, M_DIM);
     gemm(sketched_b_vec, sketching_matrix, b_vec, D_DIM, 1, M_DIM);
-    x_sketched = householderQRLS(sketched_a_matrix, sketched_b_vec, D_DIM, N_DIM);
+    int sketching_cycles = read_cycles() - sketching_start;
+
+    int solver_start = read_cycles();
+    x_ls = householderQRLS(sketched_a_matrix, sketched_b_vec, D_DIM, N_DIM);
+    int solving_cycles = read_cycles() - solver_start;
+
+    printf("Matrix dim: %d by %d, sketching took %lu cycles, solving took %lu cycles\n", D_DIM, N_DIM, sketching_cycles, solving_cycles);
     printf("\n");
-    printf("\n\n");
     // printf("qrls result x:");
     // for (int i = 0; i < N_DIM; i++) {
     //     printf("%lf ", x_ls[i]);
@@ -24,11 +30,6 @@ int main() {
     printf("sketched result x:");
     for (int i = 0; i < N_DIM; i++) {
         printf("%lf ", x_ls[i]);
-    }
-    printf("\n");
-    printf("expected x:");
-    for (int i = 0; i < N_DIM; i++) {
-        printf("%lf ", x_vec[i]);
     }
     printf("\n");
     free(x_ls);
