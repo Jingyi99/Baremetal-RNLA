6901dc0e5e5ecf132f24f6d86f76fa5336e65222
Jingyi Xu <jingyixu@berkeley.edu>
2024-12-11 16:34:39 -0800

    Add scripts to convert suitsparse matrices

---
+++ dataset/genSuitsparse.py
@@ -0,0 +1,32 @@
+import numpy as np
+import scipy.io as sio
+import scipy.sparse as sp
+from util import *
+
+# Read from MAT file
+def read_mat(filename):
+    info = sio.loadmat(filename)
+    mat_A = info['Problem']['A']
+    a_csc = mat_A[0][0]
+    a_data = a_csc.data
+    a_indices = a_csc.indices
+    a_indptr = a_csc.indptr
+
+    m_dim = a_csc.shape[0]
+    n_dim = a_csc.shape[1]
+    nnz = len(a_data)
+    return a_data, a_indices, a_indptr, m_dim, n_dim, nnz
+
+# assume a is csc format
+def generate_sparse_only(filename):
+    a_data, a_indices, a_indptr, m_dim, n_dim, nnz = read_mat(filename)
+    b_vec = np.random.rand(m_dim)
+    print_header2(m_dim, n_dim)
+    print_array('static int a_matrix_indptr', a_indptr, n_dim+1)
+    print_array('static int a_matrix_indices', a_indices, nnz)
+    print_array('static data_t a_matrix_data', a_data, nnz)
+    print_array('static data_t b_vec', b_vec.flatten(), m_dim)
+
+filename = 'mk12-b2.mat'
+generate_sparse_only(filename)
+
--- dataset/util.py
+++ dataset/util.py
@@ -1,11 +1,11 @@
 
-def print_array(name, data, data_size, data_type='double', data_fmt='{}', fold=10):
+def print_array(name, data, data_size, data_type='float', data_fmt='{}', fold=10):
     print(f"{name} [{data_size}] = {{")
     for i in range(0, len(data), fold):
         print('  ', ', '.join(data_fmt.format(x) for x in data[i:i+fold]), ',', sep='')
     print('};')
 
-def print_header2(m_dim, n_dim, dtype):
+def print_header2(m_dim, n_dim, dtype='float'):
     print(f'''
 #define M_DIM {m_dim}
 #define N_DIM {n_dim}
