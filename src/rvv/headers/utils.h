b7b9be1a863ab31a366020b77ccc51f3abd0c7f9
kevindna <kevanderson49@gmail.com>
2024-12-14 00:31:09 -0800

    uncomment utils.h

--- src/rvv/headers/utils.h
+++ src/rvv/headers/utils.h
@@ -1,21 +1,27 @@
 #pragma once 
 
-// #ifdef DEBUG
-//   void print_vector(char c, uint64_t* v, size_t vl) {
-//     printf("%c = ", c);
-//     for(int x=0; x < vl; x++) {
-//       printf("%d ", v[x]);
-//     }
-//     printf("\n");
-//   }
+void print_vector_uint(uint64_t* v, uint32_t len) {
+  // printf("%c = ", c);
+  for(int x=0; x < len; x++) {
+    printf("%d ", v[x]);
+  }
+  printf("\n");
+}
 
-//   void print_matrix(float* A, int r, int c) {
-//     uint32_t i, j;
-//     for(i = 0; i < r; i++) {
-//       for(j = 0; j < c-1; j++) {
-//         printf("%.2f ", *(A + i*c + j));
-//       }
-//       printf("%.2f\n", *(A + i*c + j));
-//     }
-//   }
-// #endif
+void print_vector(float* v, uint32_t len) {
+  // printf("%c = ", c);
+  for(int x=0; x < len; x++) {
+    printf("%f ", v[x]);
+  }
+  printf("\n");
+}
+
+void print_matrix(float* A, int r, int c) {
+  uint32_t i, j;
+  for(i = 0; i < r; i++) {
+    for(j = 0; j < c-1; j++) {
+      printf("%.2f ", *(A + i*c + j));
+    }
+    printf("%.2f\n", *(A + i*c + j));
+  }
+}
